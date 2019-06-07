0f72c943c737df87fd69ddbcf6dbc2f2059317b1
nodebotanist <the@nodebotanist>
2019-06-07 13:25:08 +0100

    Made two workers; one for upload and one for API

---
+++ api/index.js
@@ -0,0 +1,14 @@
+let router = require('./router')
+let data = require('./data-centers.json')
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Fetch and log a request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  return new Response('Hello worker!', { status: 200 })
+}
