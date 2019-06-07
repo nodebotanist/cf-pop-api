732cebba96af8ea4a403d30f266086b68691f840
nodebotanist <the@nodebotanist>
2019-06-07 13:36:52 +0100

    Uploader works

--- upload-data-centers/index.js
+++ upload-data-centers/index.js
@@ -1,3 +1,5 @@
+const dataCenters = require('./data-centers.json')
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -7,5 +9,6 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', { status: 200 })
+  DataCenterKV.put('centers', JSON.stringify(dataCenters))
+  return new Response(JSON.stringify(dataCenters), { status: 200 })
 }
